f2caa7be6473ecd3576c8895bcaac4ca1b0fb56b
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2023-05-04 21:29:43 +0900

    add function method

--- hello_pygame.py
+++ hello_pygame.py
@@ -1,22 +1,23 @@
 import pygame
 
-pygame.init()
-width, height = 600, 400
-screen = pygame.display.set_mode((width, height))
-font_size = 50
-font_file = None
-antialias = True
-font = pygame.font.Font(font_file, font_size)
-text_image = font.render("hello, pygame", antialias, pygame.Color("green"))
-player_image = pygame.image.load("doraemon.jpg").convert()
 
-while True:
-    pygame.event.clear()
-    key_pressed = pygame.key.get_pressed()
-    if key_pressed[pygame.K_ESCAPE]:
-        break
-    mouse_pos = pygame.mouse.get_pos()
+def init_screen():
+    pygame.init()
+    width, height = 600, 400
+    screen = pygame.display.set_mode((width, height))
+    return screen
 
+
+def create_text():
+    font_size = 50
+    font_file = None
+    antialias = True
+    font = pygame.font.Font(font_file, font_size)
+    text_image = font.render("hello, pygame", antialias, pygame.Color("green"))
+    return text_image
+
+
+def draw(screen, player_image, text_image, mouse_pos):
     screen.fill(pygame.Color("black"))
     screen.blit(player_image, mouse_pos)
     mouse_x, mouse_y = mouse_pos
@@ -24,4 +25,21 @@ while True:
     screen.blit(text_image, (mouse_x + text_offset_x, mouse_y))
     pygame.display.update()
 
-pygame.quit()
+
+def main():
+    screen = init_screen()
+    text_image = create_text()
+    player_image = pygame.image.load("doraemon.jpg").convert()
+
+    while True:
+        pygame.event.clear()
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[pygame.K_ESCAPE]:
+            break
+        mouse_pos = pygame.mouse.get_pos()
+        draw(screen, player_image, text_image, mouse_pos)
+
+    pygame.quit()
+
+
+main()
