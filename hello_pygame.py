475880af484c11150465e65afdfdd4b488d7b381
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2023-05-04 16:25:24 +0900

    first commit

---
+++ hello_pygame.py
@@ -0,0 +1,8 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+pygame.draw.circle(screen, pygame.Color("red"), (300, 00), 30)
+pygame.display.update()
+pygame.time.wait(5000)
+pygame.quit()
