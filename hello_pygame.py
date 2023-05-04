acb891c78bd57a6f30b208a6ab18436134aa3a43
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2023-05-04 22:10:26 +0900

    add animation

--- hello_pygame.py
+++ hello_pygame.py
@@ -17,6 +17,16 @@ def create_text():
     return text_image
 
 
+def create_player():
+    player_images = [
+        pygame.image.load("/home/yamaguchi/pygame/GDevelop-Examples/animation-speed-scale/p1_walk04.png").convert(),
+        pygame.image.load("/home/yamaguchi/pygame/GDevelop-Examples/animation-speed-scale/p1_walk05.png").convert(),
+        pygame.image.load("/home/yamaguchi/pygame/GDevelop-Examples/animation-speed-scale/p1_walk06.png").convert(),
+        pygame.image.load("/home/yamaguchi/pygame/GDevelop-Examples/animation-speed-scale/p1_walk07.png").convert()
+
+    ]
+    return player_images
+
 def draw(screen, player_image, text_image, mouse_pos):
     screen.fill(pygame.Color("black"))
     screen.blit(player_image, mouse_pos)
@@ -29,7 +39,8 @@ def draw(screen, player_image, text_image, mouse_pos):
 def main():
     screen = init_screen()
     text_image = create_text()
-    player_image = pygame.image.load("/home/yamaguchi/pygame/GDevelop-Examples/animation-speed-scale/p1_walk04.png").convert()
+    player_images = create_player()
+    frame_index = 0
 
     while True:
         should_quit = False
@@ -39,10 +50,15 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     should_quit = True
+                elif event.key == pygame.K_b:
+                    pass
         if should_quit:
             break
         mouse_pos = pygame.mouse.get_pos()
-        draw(screen, player_image, text_image, mouse_pos)
+
+        frame_index += 1
+        animation_index = frame_index % len(player_images)
+        draw(screen, player_images[animation_index], text_image, mouse_pos)
 
     pygame.quit()
 
