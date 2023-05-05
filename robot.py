36f2e32277e0dcc0f47c5664722ba25ab2a38956
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2023-05-05 19:20:21 +0900

    add landmark

--- robot.py
+++ robot.py
@@ -26,6 +26,17 @@ class Robot:
         radius = 3
         pygame.draw.circle(screen, pygame.Color(self.color), (self.x, self.y), radius)
 
+class Landmark:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, screen):
+        radius = 5
+        pygame.draw.rect(screen, pygame.Color("black"), (self.x, self.y, self.width, self.height), radius)
+
 
 
 def main():
@@ -34,14 +45,24 @@ def main():
     screen = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
 
-    robot1 = Robot(x = 450, y = 300, yaw = 0, v = 100, w = 1, dt = 0.01, color = "red")
-    robot2 = Robot(x = 450, y = 300, yaw = 0, v = 100, w = 1, dt = 0.01, color = "blue")
+    landmark_list = []
+
+    robot1 = Robot(x = 450, y = 200, yaw = 0, v = 100, w = 1, dt = 0.01, color = "red")
+    robot2 = Robot(x = 450, y = 200, yaw = 0, v = 100, w = 1, dt = 0.01, color = "blue")
 
     screen.fill(pygame.Color("white"))
 
     while True:
         frames_per_second = 60
         clock.tick(frames_per_second)
+        landmark1 = Landmark(100, 500, 10, 10)
+        landmark2 = Landmark(250, 100, 10, 10)
+        landmark3 = Landmark(700, 500, 10, 10)
+        landmark_list.append(landmark1)
+        landmark_list.append(landmark2)
+        landmark_list.append(landmark3)
+        for landmark in landmark_list:
+            landmark.draw(screen)
         robot1.update()
         robot1.noise()
         robot1.draw(screen)
