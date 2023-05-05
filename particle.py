51f182cd49d5d34e643121499a5f0e95281b4449
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2023-05-05 11:45:14 +0900

    add ball simulation

--- particle.py
+++ particle.py
@@ -1,10 +1,18 @@
+import random
 import pygame
+
 
 def main():
     pygame.init()
     width, height = 600, 400
     screen = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
+
+    dt = 1.0
+    gy = 0.5
+    particle_is_alive = False
+    x, y = 0, 0
+    vx, vy = 0, 0
 
     while True:
         frames_per_second = 60
@@ -17,9 +25,24 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     should_quit = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    particle_is_alive = True
+                    x, y = event.pos
+                    vx = random.uniform(-10, 10)
+                    vy = random.uniform(-10, 0)
         if should_quit:
             break
+
+        if particle_is_alive:
+            vy += gy * dt
+            x += vx * dt
+            y += vy * dt
+
         screen.fill(pygame.Color("black"))
+        if particle_is_alive:
+            radius = 10
+            pygame.draw.circle(screen, pygame.Color("green"), (x, y), radius)
         pygame.display.update()
 
     pygame.quit()
