f1fc2e638d8dbc62fbee8d91d5cefc70445d26b7
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2023-05-06 14:22:08 +0900

    update program

--- sim.py
+++ sim.py
@@ -1,19 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import random
+import math
+class Robot:
+    def __init__(self, x, y, yaw, v, w, dt, color):
+        self. x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+        self.w = w
+        self.dt = dt
+        self.color = color
 
-fig = plt.figure()
-ax = fig.add_subplot(111, aspect=1)
+    def noise(self):
+        self.x += random.uniform(-2, 2)
+        self.y += random.uniform(-2, 2)
 
-theta = np.linspace(0, 2*np.pi, 128)
+    def update(self):
+        self.x += self.v * math.cos(self.yaw) * self.dt
+        self.y += self.v * math.sin(self.yaw) * self.dt
+        self.yaw += self.w * self.dt
+        # self.noise()
 
-def update(f):
-    ax.cla() # ax をクリア
-    ax.grid()
-    ax.plot(np.cos(theta), np.sin(theta), c="gray")
-    ax.plot(np.cos(f), np.sin(f), "o", c="red")
+robot1 = Robot(x = 0, y = 0, yaw = 0, v = 1000, w = 10, dt = 0.01, color = "red")
 
-anim = FuncAnimation(fig, update, frames=np.pi*np.arange(0,2,0.25), interval=200)
+plot_size_x = 100
+plot_size_y = 100
+sim_time_step = 0.1
 
-plt.show()
-anim.save('Animation_線形.gif', writer='pillow')
+while True:
+    plt.clf()
+    plt.xlim(-500, 500)
+    plt.ylim(-500, 500)
+    plt.grid(which='major', color='black', linestyle='-')
+    # plt.grid(which='minor', color='black', linestyle='-')
+    # for i in range(len(self.landmarks)):
+    #     plt.plot(self.landmarks[i][0], self.landmarks[i][1], marker='o', color='black', markersize=30)
+    # for i in range(len(measurements)):
+    #     if measurements[i][0] > 0.0:
+    #         mx = measurements[i][0] * math.cos(yaw + measurements[i][1]) + x
+    #         my = measurements[i][0] * math.sin(yaw + measurements[i][1]) + y
+    #         plt.plot(mx, my, marker='o', color='red', markersize=20)
+    # plt.plot(self.gt_x, self.gt_y, marker='o', color='black', markersize=30)
+    robot1.update()
+    robot1.noise()
+    plt.plot(robot1.x, robot1.y, marker='o', color='green', markersize=20)
+    plt.pause(sim_time_step)
